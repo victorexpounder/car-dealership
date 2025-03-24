c48e414e599a3d596e773ca535c7bd25767fe7af
Eze Victor Expounder <victorexpounder@gmail.com>
2025-03-24 20:01:19 +0100

    fixing icon and images issues

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    loader: "akamai",
+    path: "/",
     remotePatterns:[
       {
         protocol: 'https',
