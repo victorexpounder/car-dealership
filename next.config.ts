242382d83726dcb4d2345201c996ad6504ad3f45
Eze Victor Expounder <victorexpounder@gmail.com>
2025-03-24 21:33:24 +0100

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    loader: "akamai",
-    path: "/",
+    
     remotePatterns:[
       {
         protocol: 'https',
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
